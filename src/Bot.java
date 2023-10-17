269ffd1cea2707ad64c781fec5f296687c1360c5
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2023-10-17 18:08:20 +0700

    feat: add minimax bot

--- src/Bot.java
+++ src/Bot.java
@@ -1,4 +1,15 @@
 import javafx.scene.control.Button;
 public abstract class Bot {
     public abstract int[] move(Button[][] tiles, int roundLeft);
+    public Button[][] copyBoardGame(Button[][] board) {
+        Button[][] tiles2 = new Button[8][8];
+
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                tiles2[x][y] = new Button();
+                tiles2[x][y].setText(board[x][y].getText());
+            }
+        }
+        return tiles2;
+    }
 }
